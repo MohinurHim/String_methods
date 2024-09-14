684459ae02adac872c8d85dd1c8ac8bd9922b1e1
MohinurHim <himmatovamohinur@gmail.com>
2024-09-14 20:07:30 +0300

    Add files via upload
    Organization of programs and string methods

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+#my_string = input('My name is Mohinur')
+print('My name is Mohinur'.upper())
+print('My name is Mohinur'.lower())
+print('My name is Mohinur'.replace(' ',''))
+print('My name is Mohinur'.replace('My name is Mohinur','M'))
+print('My name is Mohinur'.replace('My name is Mohinur','r'))
